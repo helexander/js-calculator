d70c4400fb56d44a6e8cf6720f08fe2088eccdf2
helexander <jeremychee97@gmail.com>
2021-09-29 17:20:15 +1000

    initial commit

---
+++ js/calculator.js
@@ -0,0 +1,38 @@
+// TO DO
+// ------
+// using reduce array method to accumulate functions
+// resetting input when user presses an operator button
+
+// input bar
+// 7 8 9 +
+// 4 5 6 -
+// 1 2 3 *
+// 0 0 . /
+// =====
+
+const currInput = document.getElementById("currInput").value;
+const calcButton = document.getElementById("calculateButton");
+
+
+
+console.log(currInput);
+
+function sum() {
+
+}
+
+function minus() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function calc() {
+
+}
