2f56209650be9683bb5cf179c10c15c5e85cdb32
helexander <jeremychee97@gmail.com>
2021-09-30 14:53:54 +1000

    Added more template function skeletons

--- js/calculator.js
+++ js/calculator.js
@@ -3,7 +3,9 @@
 // using reduce array method to accumulate functions
 // resetting input when user presses an operator button
 
+// --prev-val
 // input bar
+// A C Delete 
 // 7 8 9 +
 // 4 5 6 -
 // 1 2 3 *
@@ -11,28 +13,45 @@
 // =====
 
 const currInput = document.getElementById("currInput").value;
+
 const calcButton = document.getElementById("calculateButton");
 
+calcButton.addEventListener("click", () => {
+    alert(currInput);
+});
 
 
-console.log(currInput);
+// Delete
+function delNum() {
 
+}
+
+// AC
+function clear() {
+
+}
+
+// +
 function sum() {
 
 }
 
+// -
 function minus() {
 
 }
 
+// *
 function multiply() {
 
 }
 
+// /
 function divide() {
 
 }
 
+// =
 function calc() {
 
 }
